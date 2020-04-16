55dc8bbef943bd9a6141e2536d7e4bd168742a6b
Kristof Keppens <kkeppens@gmail.com>
2020-04-16 10:40:32 +0200

    merge lti changes to new 8.x version

--- modules/lti/src/main/java/org/opencastproject/lti/LtiServlet.java
+++ modules/lti/src/main/java/org/opencastproject/lti/LtiServlet.java
@@ -21,15 +21,28 @@
 
 package org.opencastproject.lti;
 
+import org.opencastproject.kernel.security.OAuthConsumerDetailsService;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.apache.commons.lang3.StringUtils;
-import org.json.simple.JSONObject;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth.common.signature.SharedConsumerSecretImpl;
+import org.springframework.security.oauth.provider.ConsumerDetails;
+import org.springframework.security.oauth.provider.ConsumerDetailsService;
+import org.tsugi.basiclti.BasicLTIConstants;
+import org.tsugi.basiclti.BasicLTIUtil;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLDecoder;
 import java.util.Arrays;
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.SortedSet;
@@ -46,11 +59,24 @@ import javax.ws.rs.core.UriBuilder;
  * A servlet to accept an LTI login via POST. The actual authentication happens in LtiProcessingFilter. GET requests
  * produce JSON containing the LTI parameters passed during LTI launch.
  */
-public class LtiServlet extends HttpServlet {
+public class LtiServlet extends HttpServlet implements ManagedService {
 
-  private static final String LTI_CUSTOM_PREFIX = "custom_";
-  private static final String LTI_CUSTOM_TOOL = "custom_tool";
-  private static final String LTI_CUSTOM_TEST = "custom_test";
+  /** Our custom parameters **/
+  private static final String TEST = "test";
+
+  private static final String CUSTOM_TOOL = BasicLTIConstants.CUSTOM_PREFIX + "tool";
+  private static final String CUSTOM_DL_TOOL = BasicLTIConstants.CUSTOM_PREFIX + "dl_tool";
+  private static final String CUSTOM_TEST = BasicLTIConstants.CUSTOM_PREFIX + TEST;
+
+  /** needed to keep track of which secret to sign ContentItemSelection message **/
+  private static final String CONSUMER_KEY = "consumer_key";
+  private static final String OAUTH_CONSUMER_KEY = BasicLTIConstants.OAUTH_PREFIX + CONSUMER_KEY;
+
+  /** URI where tools should post to in order to generate ContentItemSelection message **/
+  private static final String CONTENT_ITEMS_URI = "/lti/ci";
+
+  /** LTI parameter not specified in tsugi library **/
+  private static final String CONTENT_ITEMS = "content_items";
 
   /** The logger */
   private static final Logger logger = LoggerFactory.getLogger(LtiServlet.class);
@@ -66,124 +92,45 @@ public class LtiServlet extends HttpServlet {
 
   // The following LTI launch parameters are made available to GET requests at the /lti endpoint.
   // See https://www.imsglobal.org/specs/ltiv1p2/implementation-guide for the meaning of each.
-
-  /** See the LTI specification */
-  public static final String LTI_MESSAGE_TYPE = "lti_message_type";
-
-  /** See the LTI specification */
-  public static final String LTI_VERSION = "lti_version";
-
-  /** See the LTI specification */
-  public static final String RESOURCE_LINK_ID = "resource_link_id";
-
-  /** See the LTI specification */
-  public static final String RESOURCE_LINK_TITLE = "resource_link_title";
-
-  /** See the LTI specification */
-  public static final String RESOURCE_LINK_DESCRIPTION = "resource_link_description";
-
-  /** See the LTI specification */
-  public static final String USER_ID = "user_id";
-
-  /** See the LTI specification */
-  public static final String USER_IMAGE = "user_image";
-
-  /** See the LTI specification */
-  public static final String ROLES = "roles";
-
-  /** See the LTI specification */
-  public static final String GIVEN_NAME = "lis_person_name_given";
-
-  /** See the LTI specification */
-  public static final String FAMILY_NAME = "lis_person_name_family";
-
-  /** See the LTI specification */
-  public static final String FULL_NAME = "lis_person_name_full";
-
-  /** See the LTI specification */
-  public static final String EMAIL = "lis_person_contact_email_primary";
-
-  /** See the LTI specification */
-  public static final String CONTEXT_ID = "context_id";
-
-  /** See the LTI specification */
-  public static final String CONTEXT_TYPE = "context_type";
-
-  /** See the LTI specification */
-  public static final String CONTEXT_TITLE = "context_title";
-
-  /** See the LTI specification */
-  public static final String CONTEXT_LABEL = "context_label";
-
-  /** See the LTI specification */
-  public static final String LOCALE = "launch_presentation_locale";
-
-  /** See the LTI specification */
-  public static final String TARGET = "launch_presentation_document_target";
-
-  /** See the LTI specification */
-  public static final String WIDTH = "launch_presentation_width";
-
-  /** See the LTI specification */
-  public static final String HEIGHT = "launch_presentation_height";
-
-  /** See the LTI specification */
-  public static final String RETURN_URL = "launch_presentation_return_url";
-
-  /** See the LTI specification */
-  public static final String CONSUMER_GUID = "tool_consumer_instance_guid";
-
-  /** See the LTI specification */
-  public static final String CONSUMER_NAME = "tool_consumer_instance_name";
-
-  /** See the LTI specification */
-  public static final String CONSUMER_DESCRIPTION = "tool_consumer_instance_description";
-
-  /** See the LTI specification */
-  public static final String CONSUMER_URL = "tool_consumer_instance_url";
-
-  /** See the LTI specification */
-  public static final String CONSUMER_CONTACT = "tool_consumer_instance_contact_email";
-
-  /** See the LTI specification */
-  public static final String COURSE_OFFERING = "lis_course_offering_sourcedid";
-
-  /** See the LTI specification */
-  public static final String COURSE_SECTION = "lis_course_section_sourcedid";
-
   public static final SortedSet<String> LTI_CONSTANTS;
 
   static {
     LTI_CONSTANTS = new TreeSet<String>();
-    LTI_CONSTANTS.add(LTI_MESSAGE_TYPE);
-    LTI_CONSTANTS.add(LTI_VERSION);
-    LTI_CONSTANTS.add(RESOURCE_LINK_ID);
-    LTI_CONSTANTS.add(RESOURCE_LINK_TITLE);
-    LTI_CONSTANTS.add(RESOURCE_LINK_DESCRIPTION);
-    LTI_CONSTANTS.add(USER_ID);
-    LTI_CONSTANTS.add(USER_IMAGE);
-    LTI_CONSTANTS.add(ROLES);
-    LTI_CONSTANTS.add(GIVEN_NAME);
-    LTI_CONSTANTS.add(FAMILY_NAME);
-    LTI_CONSTANTS.add(FULL_NAME);
-    LTI_CONSTANTS.add(EMAIL);
-    LTI_CONSTANTS.add(CONTEXT_ID);
-    LTI_CONSTANTS.add(CONTEXT_TYPE);
-    LTI_CONSTANTS.add(CONTEXT_TITLE);
-    LTI_CONSTANTS.add(CONTEXT_LABEL);
-    LTI_CONSTANTS.add(LOCALE);
-    LTI_CONSTANTS.add(TARGET);
-    LTI_CONSTANTS.add(WIDTH);
-    LTI_CONSTANTS.add(HEIGHT);
-    LTI_CONSTANTS.add(RETURN_URL);
-    LTI_CONSTANTS.add(CONSUMER_GUID);
-    LTI_CONSTANTS.add(CONSUMER_NAME);
-    LTI_CONSTANTS.add(CONSUMER_DESCRIPTION);
-    LTI_CONSTANTS.add(CONSUMER_URL);
-    LTI_CONSTANTS.add(CONSUMER_CONTACT);
-    LTI_CONSTANTS.add(COURSE_OFFERING);
-    LTI_CONSTANTS.add(COURSE_SECTION);
+    LTI_CONSTANTS.add(BasicLTIConstants.LTI_MESSAGE_TYPE);
+    LTI_CONSTANTS.add(BasicLTIConstants.LTI_VERSION);
+    LTI_CONSTANTS.add(BasicLTIConstants.RESOURCE_LINK_ID);
+    LTI_CONSTANTS.add(BasicLTIConstants.RESOURCE_LINK_TITLE);
+    LTI_CONSTANTS.add(BasicLTIConstants.RESOURCE_LINK_DESCRIPTION);
+    LTI_CONSTANTS.add(BasicLTIConstants.USER_ID);
+    LTI_CONSTANTS.add(BasicLTIConstants.USER_IMAGE);
+    LTI_CONSTANTS.add(BasicLTIConstants.ROLES);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_PERSON_NAME_GIVEN);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_PERSON_NAME_FAMILY);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_PERSON_NAME_FULL);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_PERSON_CONTACT_EMAIL_PRIMARY);
+    LTI_CONSTANTS.add(BasicLTIConstants.CONTEXT_ID);
+    LTI_CONSTANTS.add(BasicLTIConstants.CONTEXT_TYPE);
+    LTI_CONSTANTS.add(BasicLTIConstants.CONTEXT_TITLE);
+    LTI_CONSTANTS.add(BasicLTIConstants.CONTEXT_LABEL);
+    LTI_CONSTANTS.add(BasicLTIConstants.LAUNCH_PRESENTATION_LOCALE);
+    LTI_CONSTANTS.add(BasicLTIConstants.LAUNCH_PRESENTATION_DOCUMENT_TARGET);
+    LTI_CONSTANTS.add(BasicLTIConstants.LAUNCH_PRESENTATION_WIDTH);
+    LTI_CONSTANTS.add(BasicLTIConstants.LAUNCH_PRESENTATION_HEIGHT);
+    LTI_CONSTANTS.add(BasicLTIConstants.LAUNCH_PRESENTATION_RETURN_URL);
+    LTI_CONSTANTS.add(BasicLTIConstants.TOOL_CONSUMER_INSTANCE_GUID);
+    LTI_CONSTANTS.add(BasicLTIConstants.TOOL_CONSUMER_INSTANCE_NAME);
+    LTI_CONSTANTS.add(BasicLTIConstants.TOOL_CONSUMER_INSTANCE_DESCRIPTION);
+    LTI_CONSTANTS.add(BasicLTIConstants.TOOL_CONSUMER_INSTANCE_URL);
+    LTI_CONSTANTS.add(BasicLTIConstants.TOOL_CONSUMER_INSTANCE_CONTACT_EMAIL);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_COURSE_OFFERING_SOURCEDID);
+    LTI_CONSTANTS.add(BasicLTIConstants.LIS_COURSE_SECTION_SOURCEDID);
+    LTI_CONSTANTS.add(BasicLTIConstants.DATA);
+    LTI_CONSTANTS.add(BasicLTIConstants.CONTENT_ITEM_RETURN_URL);
+    LTI_CONSTANTS.add(BasicLTIConstants.ACCEPT_PRESENTATION_DOCUMENT_TARGETS);
+    LTI_CONSTANTS.add(OAUTH_CONSUMER_KEY);
   }
+
+  private OAuthConsumerDetailsService consumerDetailsService;
 
   /**
    * {@inheritDoc}
@@ -195,14 +142,35 @@ public class LtiServlet extends HttpServlet {
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     // Store the LTI data as a map in the session
     HttpSession session = req.getSession(false);
+
+    // Always set the session cookie
+    resp.setHeader("Set-Cookie", "JSESSIONID=" + session.getId() + ";Path=/");
+
+    // Send content item (deep linking) message back to LMS
+    if (CONTENT_ITEMS_URI.equals(req.getRequestURI())) {
+      sendContentItem(req, resp);
+      return;
+    }
+
     session.setAttribute(SESSION_ATTRIBUTE_KEY, getLtiValuesAsMap(req));
 
-    // We must return a 200 for some OAuth client libraries to accept this as a valid response
+    String messageType = StringUtils.trimToEmpty(req.getParameter(BasicLTIConstants.LTI_MESSAGE_TYPE));
 
     // The URL of the LTI tool. If no specific tool is passed we use the test tool
-    UriBuilder builder = null;
+    UriBuilder builder;
     try {
-      URI toolUri = new URI(StringUtils.trimToEmpty(req.getParameter(LTI_CUSTOM_TOOL)));
+      // If a content item request, use the dl_tool instead of tool so that we can
+      // return a custom tool param in the result later
+      logger.debug("Received '{}' LTI message type", messageType);
+
+      URI toolUri;
+      if (messageType.equals(BasicLTIConstants.LTI_MESSAGE_TYPE_CONTENTITEMSELECTIONREQUEST)) {
+        toolUri = new URI(URLDecoder.decode(StringUtils.trimToEmpty(
+                req.getParameter(CUSTOM_DL_TOOL)), "UTF-8"));
+      } else {
+        toolUri = new URI(URLDecoder.decode(StringUtils.trimToEmpty(
+                req.getParameter(CUSTOM_TOOL)), "UTF-8"));
+      }
 
       if (toolUri.getPath().isEmpty())
         throw new URISyntaxException(toolUri.toString(), "Provided 'custom_tool' has an empty path");
@@ -218,32 +186,40 @@ public class LtiServlet extends HttpServlet {
         builder = UriBuilder.fromUri(toolUri).scheme(null).host(null).userInfo(null).port(-1);
       }
     } catch (URISyntaxException ex) {
-      logger.warn("The 'custom_tool' parameter was invalid: '{}'. Reverting to default: '{}'",
-              Arrays.toString(req.getParameterValues(LTI_CUSTOM_TOOL)), TOOLS_URL);
+      logger.warn("The 'custom_tool' parameter was invalid: '{}'. Reverting to default: '{}'. Got syntaxerror: '{}'",
+              Arrays.toString(req.getParameterValues(CUSTOM_TOOL)), TOOLS_URL, ex);
       builder = UriBuilder.fromPath(TOOLS_URL);
     }
 
     // We need to add the custom params to the outgoing request
-    for (String key : req.getParameterMap().keySet()) {
-      logger.debug("Found query parameter '{}'", key);
-      if (key.startsWith(LTI_CUSTOM_PREFIX) && (!LTI_CUSTOM_TOOL.equals(key))) {
+    for (Object k : req.getParameterMap().keySet()) {
+      String key = k.toString();
+      logger.debug("Found query parameter '{}'", k);
+      if (key.startsWith(BasicLTIConstants.CUSTOM_PREFIX) && !CUSTOM_TOOL.equals(key)
+              && !CUSTOM_DL_TOOL.equals(key)) {
         String paramValue = req.getParameter(key);
         // we need to remove the prefix custom_
-        String paramName = key.substring(LTI_CUSTOM_PREFIX.length());
+        String paramName = key.substring(BasicLTIConstants.CUSTOM_PREFIX.length());
         logger.debug("Found custom var: {}:{}", paramName, paramValue);
         builder.queryParam(paramName, paramValue);
       }
     }
 
+    // add params required for content item
+    if (messageType.equals(BasicLTIConstants.LTI_MESSAGE_TYPE_CONTENTITEMSELECTIONREQUEST)) {
+      if (req.getParameterMap().containsKey(BasicLTIConstants.DATA)) {
+        builder.queryParam(BasicLTIConstants.DATA, req.getParameter(BasicLTIConstants.DATA));
+      }
+      builder.queryParam(CONSUMER_KEY, req.getParameter(BasicLTIConstants.OAUTH_PREFIX + CONSUMER_KEY));
+      builder.queryParam(BasicLTIConstants.CONTENT_ITEM_RETURN_URL, req.getParameter(BasicLTIConstants.CONTENT_ITEM_RETURN_URL));
+    }
+
     // Build the final URL (as a string)
     String redirectUrl = builder.build().toString();
 
-    // Always set the session cookie
-    resp.setHeader("Set-Cookie", "JSESSIONID=" + session.getId() + ";Path=/");
-
     // The client can specify debug option by passing a value to test
     // if in test mode display details where we go
-    if (Boolean.valueOf(StringUtils.trimToEmpty(req.getParameter(LTI_CUSTOM_TEST)))) {
+    if (Boolean.valueOf(StringUtils.trimToEmpty(req.getParameter(CUSTOM_TEST)))) {
       resp.setContentType("text/html");
       resp.getWriter().write("<html><body>Welcome to Opencast LTI; you are going to " + redirectUrl + "<br>");
       resp.getWriter().write("<a href=\"" + redirectUrl + "\">continue...</a></body></html>");
@@ -252,6 +228,39 @@ public class LtiServlet extends HttpServlet {
       logger.debug(redirectUrl);
       resp.sendRedirect(redirectUrl);
     }
+  }
+
+  /**
+   * Sends a ContentItemSelection response back to the LMS
+   *
+   * @param req
+   *          the HttpServletRequest
+   * @param resp
+   *          the HttpServletResponse
+   */
+  private void sendContentItem(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String consumerKey = req.getParameter(CONSUMER_KEY);
+    ConsumerDetails consumer = consumerDetailsService.loadConsumerByConsumerKey(consumerKey);
+    String consumerSecret = ((SharedConsumerSecretImpl) consumer.getSignatureSecret()).getConsumerSecret();
+
+    String contentItems = req.getParameter(CONTENT_ITEMS);
+    String returnUrl = req.getParameter(BasicLTIConstants.CONTENT_ITEM_RETURN_URL);
+
+    Map<String, String> props = new HashMap<String, String>();
+    props.put(BasicLTIConstants.LTI_MESSAGE_TYPE, BasicLTIConstants.LTI_MESSAGE_TYPE_CONTENTITEMSELECTION);
+    props.put(CONTENT_ITEMS, contentItems);
+    props.put(BasicLTIConstants.DATA, req.getParameter(BasicLTIConstants.DATA));
+    Map<String, String> properties = BasicLTIUtil.signProperties(props, returnUrl,
+            "POST", consumerKey, consumerSecret, "", "", "", "", "", null);
+    resp.setContentType("text/html");
+
+    // whether to show debug info before sending content items to tool consumer
+    boolean test = false;
+    if ("true".equals(req.getParameter(TEST))) {
+      test = true;
+    }
+
+    resp.getWriter().write(BasicLTIUtil.postLaunchHTML(properties, returnUrl, "Send content to LMS", test, null));
   }
 
   /**
@@ -291,7 +300,28 @@ public class LtiServlet extends HttpServlet {
         ltiAttributes = new HashMap<String, String>();
       }
       resp.setContentType("application/json");
-      JSONObject.writeJSONString(ltiAttributes, resp.getWriter());
+      Gson gson = new GsonBuilder().create();
+      resp.getWriter().write(gson.toJson(ltiAttributes));
     }
   }
+
+  /**
+   * Sets the consumer details service
+   *
+   * @param consumerDetailsService
+   *          the consumer details service to set
+   */
+  public void setConsumerDetailsService(ConsumerDetailsService consumerDetailsService) {
+    this.consumerDetailsService = (OAuthConsumerDetailsService) consumerDetailsService;
+  }
+
+  public void activate(ComponentContext cc) {
+    logger.info("LTI Serviet started.");
+  }
+
+  @Override
+  public void updated(Dictionary<String, ?> properties) {
+    logger.info("LTI Servlet updated.");
+  }
+
 }
